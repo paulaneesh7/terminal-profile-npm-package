c56ccf26b3905f4a0fe7dbf1ef6d9d59b3172a5a
Aneesh Paul <aneesh16117@gmail.com>
2025-01-27 23:52:09 +0530

    Changed the links

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,6 @@
 module.exports = {
     twitter: 'https://twitter.com/vincenzo7v2',
     github: 'https://github.com/paulaneesh7',
-    web: 'https://paulaneesh7.dev',
-    email: 'contact@paulaneesh7.dev'
+    web: 'https://paulaneesh7.in',
+    email: 'aneesh16117@gmail.com'
 };
